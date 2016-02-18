1bf1dd2e16f94f36ae26dd92f67f99a3fc7893be
Artem Zhdanov <artem.zhdanov@macys.com>
2016-02-18 11:51:28 -0800

    refresh

--- src/main/java/org/springframework/cloud/consul/sample/ReloadSignalHandler.java
+++ src/main/java/org/springframework/cloud/consul/sample/ReloadSignalHandler.java
@@ -1,7 +1,10 @@
 package org.springframework.cloud.consul.sample;
 
+import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.context.scope.refresh.RefreshScope;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
 import sun.misc.Signal;
 import sun.misc.SignalHandler;
 
@@ -9,10 +12,15 @@ import sun.misc.SignalHandler;
  * @author Artem Zhdanov <azhdanov@griddynamics.com>
  * @since 17.02.2016
  */
-public class ReloadSignalHandler implements SignalHandler {
+public class ReloadSignalHandler implements SignalHandler, ApplicationContextAware {
 
-    @Autowired
-    private RefreshScope refreshScope;
+    private ApplicationContext applicationContext;
+
+    @Override
+    public void setApplicationContext(final ApplicationContext applicationContext) throws BeansException {
+        this.applicationContext = applicationContext;
+    }
+
 
     private SignalHandler oldHandler;
     // Static method to install the signal handler
@@ -28,6 +36,7 @@ public class ReloadSignalHandler implements SignalHandler {
     public void handle(final Signal sig) {
         try {
             System.out.println("Worked!!!");
+            final RefreshScope refreshScope = applicationContext.getBean(RefreshScope.class);
             refreshScope.refreshAll();
             if ( oldHandler != SIG_DFL && oldHandler != SIG_IGN ) {
                 oldHandler.handle(sig);
